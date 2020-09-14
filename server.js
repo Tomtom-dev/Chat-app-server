5fc3b6ed5b8f935f775931d4d24f3e6548b961b8
Tomtom-dev <thomasheddadji0@gmail.com>
2020-09-14 17:41:46 +0200

    create mutation type and add it to resolver

--- server.js
+++ server.js
@@ -12,11 +12,27 @@ type Messages {
 type Query {
     messages:[Messages!]
 }
-`
+
+type Mutation {
+    postMessage(user: String!, content: String!): ID !
+}
+
+`;
 
 const resolvers ={
     Query:{
         messages:() =>messages,
+    },
+    Mutation: {
+        postMessage: (parent, {user, content}) =>{
+            const id= messages.length;
+            messages.push({
+                id,
+                user,
+                content,
+            });
+            return id;
+        }
     }
 }
 
